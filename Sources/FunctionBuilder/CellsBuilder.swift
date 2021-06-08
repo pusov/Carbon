0c5f3068df935ae3ea4acc05e9e9cc04eb290b6a
Pavel Usov <pavel.usov@gmail.com>
2021-06-08 06:09:34 +0300

    Расширены возможности function builder. Убран лимит на кол-во элементов в SectionBuilder и CellsBuilder

--- Sources/FunctionBuilder/CellsBuilder.swift
+++ Sources/FunctionBuilder/CellsBuilder.swift
@@ -2,7 +2,7 @@
 // swiftlint:disable function_parameter_count
 
 /// The custom parameter attribute that constructs cells from multi-statement closures.
-@_functionBuilder
+@resultBuilder
 public struct CellsBuilder: CellsBuildable {
     @usableFromInline
     internal var cellNodes: [CellNode]
@@ -13,6 +13,11 @@ public struct CellsBuilder: CellsBuildable {
         cellNodes
     }
 
+    @inlinable
+    public static func buildExpression<C: CellsBuildable>(_ c: C) -> CellsBuilder {
+        CellsBuilder(c)
+    }
+    
     /// :nodoc:
     @inlinable
     public static func buildBlock() -> CellsBuilder {
@@ -21,80 +26,36 @@ public struct CellsBuilder: CellsBuildable {
 
     /// :nodoc:
     @inlinable
-    public static func buildBlock<C: CellsBuildable>(_ c: C) -> CellsBuilder {
+    public static func buildBlock(_ c: CellsBuilder...) -> CellsBuilder {
         CellsBuilder(c)
     }
 
     /// :nodoc:
     @inlinable
-    public static func buildBlock<C0: CellsBuildable, C1: CellsBuildable>(_ c0: C0, _ c1: C1) -> CellsBuilder {
-        CellsBuilder(c0, c1)
+    public static func buildOptional(_ c: CellsBuildable?) -> CellsBuilder {
+        c.map { CellsBuilder($0) } ?? CellsBuilder()
     }
 
     /// :nodoc:
     @inlinable
-    public static func buildBlock<C0: CellsBuildable, C1: CellsBuildable, C2: CellsBuildable>(_ c0: C0, _ c1: C1, _ c2: C2) -> CellsBuilder {
-        CellsBuilder(c0, c1, c2)
+    public static func buildEither(first: CellsBuildable) -> CellsBuilder {
+        CellsBuilder(first)
     }
 
     /// :nodoc:
     @inlinable
-    public static func buildBlock<C0: CellsBuildable, C1: CellsBuildable, C2: CellsBuildable, C3: CellsBuildable>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3) -> CellsBuilder {
-        CellsBuilder(c0, c1, c2, c3)
+    public static func buildEither(second: CellsBuildable) -> CellsBuilder {
+        CellsBuilder(second)
     }
-
-    /// :nodoc:
+    
     @inlinable
-    public static func buildBlock<C0: CellsBuildable, C1: CellsBuildable, C2: CellsBuildable, C3: CellsBuildable, C4: CellsBuildable>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4) -> CellsBuilder {
-        CellsBuilder(c0, c1, c2, c3, c4)
+    public static func buildArray<C: CellsBuildable>(_ components: [C]) -> CellsBuilder {
+        CellsBuilder(components)
     }
-
-    /// :nodoc:
+    
     @inlinable
-    public static func buildBlock<C0: CellsBuildable, C1: CellsBuildable, C2: CellsBuildable, C3: CellsBuildable, C4: CellsBuildable, C5: CellsBuildable>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4, _ c5: C5) -> CellsBuilder {
-        CellsBuilder(c0, c1, c2, c3, c4, c5)
-    }
-
-    /// :nodoc:
-    @inlinable
-    public static func buildBlock<C0: CellsBuildable, C1: CellsBuildable, C2: CellsBuildable, C3: CellsBuildable, C4: CellsBuildable, C5: CellsBuildable, C6: CellsBuildable>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4, _ c5: C5, _ c6: C6) -> CellsBuilder {
-        CellsBuilder(c0, c1, c2, c3, c4, c5, c6)
-    }
-
-    /// :nodoc:
-    @inlinable
-    public static func buildBlock<C0: CellsBuildable, C1: CellsBuildable, C2: CellsBuildable, C3: CellsBuildable, C4: CellsBuildable, C5: CellsBuildable, C6: CellsBuildable, C7: CellsBuildable>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4, _ c5: C5, _ c6: C6, _ c7: C7) -> CellsBuilder {
-        CellsBuilder(c0, c1, c2, c3, c4, c5, c6, c7)
-    }
-
-    /// :nodoc:
-    @inlinable
-    public static func buildBlock<C0: CellsBuildable, C1: CellsBuildable, C2: CellsBuildable, C3: CellsBuildable, C4: CellsBuildable, C5: CellsBuildable, C6: CellsBuildable, C7: CellsBuildable, C8: CellsBuildable>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4, _ c5: C5, _ c6: C6, _ c7: C7, _ c8: C8) -> CellsBuilder {
-        CellsBuilder(c0, c1, c2, c3, c4, c5, c6, c7, c8)
-    }
-
-    /// :nodoc:
-    @inlinable
-    public static func buildBlock<C0: CellsBuildable, C1: CellsBuildable, C2: CellsBuildable, C3: CellsBuildable, C4: CellsBuildable, C5: CellsBuildable, C6: CellsBuildable, C7: CellsBuildable, C8: CellsBuildable, C9: CellsBuildable>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4, _ c5: C5, _ c6: C6, _ c7: C7, _ c8: C8, _ c9: C9) -> CellsBuilder {
-        CellsBuilder(c0, c1, c2, c3, c4, c5, c6, c7, c8, c9)
-    }
-
-    /// :nodoc:
-    @inlinable
-    public static func buildIf<C: CellsBuildable>(_ c: C?) -> C? {
-        c
-    }
-
-    /// :nodoc:
-    @inlinable
-    public static func buildEither<C: CellsBuildable>(first: C) -> C {
-        first
-    }
-
-    /// :nodoc:
-    @inlinable
-    public static func buildEither<C: CellsBuildable>(second: C) -> C {
-        second
+    public static func buildLimitedAvailability<C: CellsBuildable>(_ component: C) -> CellsBuilder {
+        CellsBuilder(component)
     }
 }
 
@@ -105,70 +66,13 @@ internal extension CellsBuilder {
     }
 
     @usableFromInline
-    init<C: CellsBuildable>(_ c: C) {
-        cellNodes = c.buildCells()
+    init(_ c: CellsBuildable...) {
+        self.init(c)
     }
-
+    
     @usableFromInline
-    init<C0: CellsBuildable, C1: CellsBuildable>(_ c0: C0, _ c1: C1) {
-        cellNodes = c0.buildCells() + c1.buildCells()
-    }
-
-    @usableFromInline
-    init<C0: CellsBuildable, C1: CellsBuildable, C2: CellsBuildable>(_ c0: C0, _ c1: C1, _ c2: C2) {
-        cellNodes = c0.buildCells() + c1.buildCells() + c2.buildCells()
-    }
-
-    @usableFromInline
-    init<C0: CellsBuildable, C1: CellsBuildable, C2: CellsBuildable, C3: CellsBuildable>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3) {
-        let cellNodes0 = c0.buildCells() + c1.buildCells() + c2.buildCells()
-        let cellNodes1 = c3.buildCells()
-        cellNodes = cellNodes0 + cellNodes1
-    }
-
-    @usableFromInline
-    init<C0: CellsBuildable, C1: CellsBuildable, C2: CellsBuildable, C3: CellsBuildable, C4: CellsBuildable>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4) {
-        let cellNodes0 = c0.buildCells() + c1.buildCells() + c2.buildCells()
-        let cellNodes1 = c3.buildCells() + c4.buildCells()
-        cellNodes = cellNodes0 + cellNodes1
-    }
-
-    @usableFromInline
-    init<C0: CellsBuildable, C1: CellsBuildable, C2: CellsBuildable, C3: CellsBuildable, C4: CellsBuildable, C5: CellsBuildable>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4, _ c5: C5) {
-        let cellNodes0 = c0.buildCells() + c1.buildCells() + c2.buildCells()
-        let cellNodes1 = c3.buildCells() + c4.buildCells() + c5.buildCells()
-        cellNodes = cellNodes0 + cellNodes1
-    }
-
-    @usableFromInline
-    init<C0: CellsBuildable, C1: CellsBuildable, C2: CellsBuildable, C3: CellsBuildable, C4: CellsBuildable, C5: CellsBuildable, C6: CellsBuildable>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4, _ c5: C5, _ c6: C6) {
-        let cellNodes0 = c0.buildCells() + c1.buildCells() + c2.buildCells()
-        let cellNodes1 = c3.buildCells() + c4.buildCells() + c5.buildCells()
-        let cellNodes2 = c6.buildCells()
-        cellNodes = cellNodes0 + cellNodes1 + cellNodes2
-    }
-
-    @usableFromInline
-    init<C0: CellsBuildable, C1: CellsBuildable, C2: CellsBuildable, C3: CellsBuildable, C4: CellsBuildable, C5: CellsBuildable, C6: CellsBuildable, C7: CellsBuildable>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4, _ c5: C5, _ c6: C6, _ c7: C7) {
-        let cellNodes0 = c0.buildCells() + c1.buildCells() + c2.buildCells()
-        let cellNodes1 = c3.buildCells() + c4.buildCells() + c5.buildCells()
-        let cellNodes2 = c6.buildCells() + c7.buildCells()
-        cellNodes = cellNodes0 + cellNodes1 + cellNodes2
-    }
-
-    @usableFromInline
-    init<C0: CellsBuildable, C1: CellsBuildable, C2: CellsBuildable, C3: CellsBuildable, C4: CellsBuildable, C5: CellsBuildable, C6: CellsBuildable, C7: CellsBuildable, C8: CellsBuildable>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4, _ c5: C5, _ c6: C6, _ c7: C7, _ c8: C8) {
-        let cellNodes0 = c0.buildCells() + c1.buildCells() + c2.buildCells()
-        let cellNodes1 = c3.buildCells() + c4.buildCells() + c5.buildCells()
-        let cellNodes2 = c6.buildCells() + c7.buildCells() + c8.buildCells()
-        cellNodes = cellNodes0 + cellNodes1 + cellNodes2
-    }
-
-    @usableFromInline
-    init<C0: CellsBuildable, C1: CellsBuildable, C2: CellsBuildable, C3: CellsBuildable, C4: CellsBuildable, C5: CellsBuildable, C6: CellsBuildable, C7: CellsBuildable, C8: CellsBuildable, C9: CellsBuildable>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4, _ c5: C5, _ c6: C6, _ c7: C7, _ c8: C8, _ c9: C9) {
-        let cellNodes0 = c0.buildCells() + c1.buildCells() + c2.buildCells()
-        let cellNodes1 = c3.buildCells() + c4.buildCells() + c5.buildCells()
-        let cellNodes2 = c6.buildCells() + c7.buildCells() + c8.buildCells()
-        cellNodes = cellNodes0 + cellNodes1 + cellNodes2 + c9.buildCells()
+    init(_ c: [CellsBuildable]) {
+        cellNodes = c.map { $0.buildCells() }.reduce([], +)
     }
 }
+
--- Sources/FunctionBuilder/SectionsBuilder.swift
+++ Sources/FunctionBuilder/SectionsBuilder.swift
@@ -2,7 +2,7 @@
 // swiftlint:disable function_parameter_count
 
 /// The custom parameter attribute that constructs sections from multi-statement closures.
-@_functionBuilder
+@resultBuilder
 public struct SectionsBuilder: SectionsBuildable {
     @usableFromInline
     internal var sections: [Section]
@@ -10,6 +10,11 @@ public struct SectionsBuilder: SectionsBuildable {
     /// Build an array of section.
     public func buildSections() -> [Section] {
         sections
+    }
+    
+    @inlinable
+    public static func buildExpression<C: SectionsBuildable>(_ s: C) -> SectionsBuilder {
+        SectionsBuilder(s)
     }
 
     /// :nodoc:
@@ -20,80 +25,36 @@ public struct SectionsBuilder: SectionsBuildable {
 
     /// :nodoc:
     @inlinable
-    public static func buildBlock<S: SectionsBuildable>(_ c: S) -> SectionsBuilder {
-        SectionsBuilder(c)
+    public static func buildBlock<S: SectionsBuildable>(_ s: S...) -> SectionsBuilder {
+        SectionsBuilder(s)
     }
 
     /// :nodoc:
     @inlinable
-    public static func buildBlock<S0: SectionsBuildable, S1: SectionsBuildable>(_ s0: S0, _ s1: S1) -> SectionsBuilder {
-        SectionsBuilder(s0, s1)
+    public static func buildOptional<S: SectionsBuildable>(_ s: S?) -> SectionsBuilder {
+        s.map { SectionsBuilder($0) } ?? SectionsBuilder()
     }
 
     /// :nodoc:
     @inlinable
-    public static func buildBlock<S0: SectionsBuildable, S1: SectionsBuildable, S2: SectionsBuildable>(_ s0: S0, _ s1: S1, _ s2: S2) -> SectionsBuilder {
-        SectionsBuilder(s0, s1, s2)
+    public static func buildEither<S: SectionsBuildable>(first: S) -> SectionsBuilder {
+        SectionsBuilder(first)
     }
 
     /// :nodoc:
     @inlinable
-    public static func buildBlock<S0: SectionsBuildable, S1: SectionsBuildable, S2: SectionsBuildable, S3: SectionsBuildable>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3) -> SectionsBuilder {
-        SectionsBuilder(s0, s1, s2, s3)
+    public static func buildEither<S: SectionsBuildable>(second: S) -> SectionsBuilder {
+        SectionsBuilder(second)
     }
-
-    /// :nodoc:
+    
     @inlinable
-    public static func buildBlock<S0: SectionsBuildable, S1: SectionsBuildable, S2: SectionsBuildable, S3: SectionsBuildable, S4: SectionsBuildable>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4) -> SectionsBuilder {
-        SectionsBuilder(s0, s1, s2, s3, s4)
+    public static func buildArray<S: SectionsBuildable>(_ components: [S]) -> SectionsBuilder {
+        SectionsBuilder(components)
     }
-
-    /// :nodoc:
+    
     @inlinable
-    public static func buildBlock<S0: SectionsBuildable, S1: SectionsBuildable, S2: SectionsBuildable, S3: SectionsBuildable, S4: SectionsBuildable, S5: SectionsBuildable>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5) -> SectionsBuilder {
-        SectionsBuilder(s0, s1, s2, s3, s4, s5)
-    }
-
-    /// :nodoc:
-    @inlinable
-    public static func buildBlock<S0: SectionsBuildable, S1: SectionsBuildable, S2: SectionsBuildable, S3: SectionsBuildable, S4: SectionsBuildable, S5: SectionsBuildable, S6: SectionsBuildable>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6) -> SectionsBuilder {
-        SectionsBuilder(s0, s1, s2, s3, s4, s5, s6)
-    }
-
-    /// :nodoc:
-    @inlinable
-    public static func buildBlock<S0: SectionsBuildable, S1: SectionsBuildable, S2: SectionsBuildable, S3: SectionsBuildable, S4: SectionsBuildable, S5: SectionsBuildable, S6: SectionsBuildable, S7: SectionsBuildable>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6, _ s7: S7) -> SectionsBuilder {
-        SectionsBuilder(s0, s1, s2, s3, s4, s5, s6, s7)
-    }
-
-    /// :nodoc:
-    @inlinable
-    public static func buildBlock<S0: SectionsBuildable, S1: SectionsBuildable, S2: SectionsBuildable, S3: SectionsBuildable, S4: SectionsBuildable, S5: SectionsBuildable, S6: SectionsBuildable, S7: SectionsBuildable, S8: SectionsBuildable>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6, _ s7: S7, _ s8: S8) -> SectionsBuilder {
-        SectionsBuilder(s0, s1, s2, s3, s4, s5, s6, s7, s8)
-    }
-
-    /// :nodoc:
-    @inlinable
-    public static func buildBlock<S0: SectionsBuildable, S1: SectionsBuildable, S2: SectionsBuildable, S3: SectionsBuildable, S4: SectionsBuildable, S5: SectionsBuildable, S6: SectionsBuildable, S7: SectionsBuildable, S8: SectionsBuildable, S9: SectionsBuildable>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6, _ s7: S7, _ s8: S8, _ s9: S9) -> SectionsBuilder {
-        SectionsBuilder(s0, s1, s2, s3, s4, s5, s6, s7, s8, s9)
-    }
-
-    /// :nodoc:
-    @inlinable
-    public static func buildIf<S: SectionsBuildable>(_ s: S?) -> S? {
-        s
-    }
-
-    /// :nodoc:
-    @inlinable
-    public static func buildEither<S: SectionsBuildable>(first: S) -> S {
-        first
-    }
-
-    /// :nodoc:
-    @inlinable
-    public static func buildEither<S: SectionsBuildable>(second: S) -> S {
-        second
+    public static func buildLimitedAvailability<S: SectionsBuildable>(_ component: S) -> SectionsBuilder {
+        SectionsBuilder(component)
     }
 }
 
@@ -104,70 +65,12 @@ internal extension SectionsBuilder {
     }
 
     @usableFromInline
-    init<S: SectionsBuildable>(_ s: S) {
-        sections = s.buildSections()
+    init<S: SectionsBuildable>(_ s: [S]) {
+        sections = s.map { $0.buildSections() }.reduce([], + )
     }
 
     @usableFromInline
-    init<S0: SectionsBuildable, S1: SectionsBuildable>(_ s0: S0, _ s1: S1) {
-        sections = s0.buildSections() + s1.buildSections()
-    }
-
-    @usableFromInline
-    init<S0: SectionsBuildable, S1: SectionsBuildable, S2: SectionsBuildable>(_ s0: S0, _ s1: S1, _ s2: S2) {
-        sections = s0.buildSections() + s1.buildSections() + s2.buildSections()
-    }
-
-    @usableFromInline
-    init<S0: SectionsBuildable, S1: SectionsBuildable, S2: SectionsBuildable, S3: SectionsBuildable>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3) {
-        let sections0 = s0.buildSections() + s1.buildSections() + s2.buildSections()
-        let sections1 = s3.buildSections()
-        sections = sections0 + sections1
-    }
-
-    @usableFromInline
-    init<S0: SectionsBuildable, S1: SectionsBuildable, S2: SectionsBuildable, S3: SectionsBuildable, S4: SectionsBuildable>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4) {
-        let sections0 = s0.buildSections() + s1.buildSections() + s2.buildSections()
-        let sections1 = s3.buildSections() + s4.buildSections()
-        sections = sections0 + sections1
-    }
-
-    @usableFromInline
-    init<S0: SectionsBuildable, S1: SectionsBuildable, S2: SectionsBuildable, S3: SectionsBuildable, S4: SectionsBuildable, S5: SectionsBuildable>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5) {
-        let sections0 = s0.buildSections() + s1.buildSections() + s2.buildSections()
-        let sections1 = s3.buildSections() + s4.buildSections() + s5.buildSections()
-        sections = sections0 + sections1
-    }
-
-    @usableFromInline
-    init<S0: SectionsBuildable, S1: SectionsBuildable, S2: SectionsBuildable, S3: SectionsBuildable, S4: SectionsBuildable, S5: SectionsBuildable, S6: SectionsBuildable>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6) {
-        let sections0 = s0.buildSections() + s1.buildSections() + s2.buildSections()
-        let sections1 = s3.buildSections() + s4.buildSections() + s5.buildSections()
-        let sections2 = s6.buildSections()
-        sections = sections0 + sections1 + sections2
-    }
-
-    @usableFromInline
-    init<S0: SectionsBuildable, S1: SectionsBuildable, S2: SectionsBuildable, S3: SectionsBuildable, S4: SectionsBuildable, S5: SectionsBuildable, S6: SectionsBuildable, S7: SectionsBuildable>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6, _ s7: S7) {
-        let sections0 = s0.buildSections() + s1.buildSections() + s2.buildSections()
-        let sections1 = s3.buildSections() + s4.buildSections() + s5.buildSections()
-        let sections2 = s6.buildSections() + s7.buildSections()
-        sections = sections0 + sections1 + sections2
-    }
-
-    @usableFromInline
-    init<S0: SectionsBuildable, S1: SectionsBuildable, S2: SectionsBuildable, S3: SectionsBuildable, S4: SectionsBuildable, S5: SectionsBuildable, S6: SectionsBuildable, S7: SectionsBuildable, S8: SectionsBuildable>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6, _ s7: S7, _ s8: S8) {
-        let sections0 = s0.buildSections() + s1.buildSections() + s2.buildSections()
-        let sections1 = s3.buildSections() + s4.buildSections() + s5.buildSections()
-        let sections2 = s6.buildSections() + s7.buildSections() + s8.buildSections()
-        sections = sections0 + sections1 + sections2
-    }
-
-    @usableFromInline
-    init<S0: SectionsBuildable, S1: SectionsBuildable, S2: SectionsBuildable, S3: SectionsBuildable, S4: SectionsBuildable, S5: SectionsBuildable, S6: SectionsBuildable, S7: SectionsBuildable, S8: SectionsBuildable, S9: SectionsBuildable>(_ s0: S0, _ s1: S1, _ s2: S2, _ s3: S3, _ s4: S4, _ s5: S5, _ s6: S6, _ s7: S7, _ s8: S8, _ s9: S9) {
-        let sections0 = s0.buildSections() + s1.buildSections() + s2.buildSections()
-        let sections1 = s3.buildSections() + s4.buildSections() + s5.buildSections()
-        let sections2 = s6.buildSections() + s7.buildSections() + s8.buildSections()
-        sections = sections0 + sections1 + sections2 + s9.buildSections()
+    init<S: SectionsBuildable>(_ s: S...) {
+        self.init(s)
     }
 }
